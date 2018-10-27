319d4d53c2c7a7ee41c112ac1748634164194099
ushiihsu <liu.ry1411@gmail.com>
2018-10-27 17:38:02 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,5 +4,5 @@
 -- Sample code is MIT licensed; see https://www.coronalabs.com/links/code/license
 ---------------------------------------------------------------------------------------
 
-local msgText = display.newText( "Hello, world!", display.contentWidth*0.5,display.contentHeight*0.25, native.systemFont, 32 )
+local msgText = display.newText( "Hello, world!", display.contentWidth*0.5,display.contentHeight*0.5, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
